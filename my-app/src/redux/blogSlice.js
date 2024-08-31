49172c6c8d9374af07dfb6c81c644e08fd89afd5
Habihirwe <habiholivier10@gmail.com>
2024-08-31 22:04:27 +0200

    added deployed link

--- my-app/src/redux/blogSlice.js
+++ my-app/src/redux/blogSlice.js
@@ -7,7 +7,7 @@ export const getPost = createAsyncThunk("blog/getPost", async (_, thunkAPI) => {
       //   throw new Error('No token found, please log in first.');
       // }
   
-      const response = await fetch("http://localhost:8001/api/getblogs", {
+      const response = await fetch("https://alumni-football-bn.onrender.com/api/getblogs", {
         method: "GET",
         headers: {
           "Content-Type": "application/json", 
